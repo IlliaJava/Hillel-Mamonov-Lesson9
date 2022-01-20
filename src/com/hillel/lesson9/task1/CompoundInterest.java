85af567ce0dbd95a29995edf38693da5f804f16e
Illia Mamonov <illia.mamonov@gmail.com>
2022-01-21 01:00:19 +0200

    Initial commit

---
+++ src/com/hillel/lesson9/task1/CompoundInterest.java
@@ -0,0 +1,13 @@
+package com.hillel.lesson9.task1;
+// Task 1. Compound Bank Interest calculator
+        /*1) В банк положили s денег. Какой станет сумма вклада через n лет,
+        если процент 1,5% добавляется к сумме вклада ежемесячно. (сложный процент)
+        Написать метод который принимает 2 параметра - s, n и возвращает сумму денег на счету.*/
+
+
+import java.util.Scanner;
+
+public class CompoundInterest {
+
+
+}
